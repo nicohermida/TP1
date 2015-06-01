14fe3837531c8cc9900f20450353e70ec788a576
Nicolas Hermida Cadel <hermida.nicolas@gmail.com>
2015-06-01 00:55:25 -0300

    Creación de estructura de llamado a funciones.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,32 @@ int main()
 
 {
 
+	Team listOfTeams[MAX_EQUIPOS];
+    
+	int option=1;
+	while(option!=0){
+		option=ShowMenu();
+		switch (option){
+			case 1:
+				AddTeam();
+				ShowMenu();
+				break;
+			case 2:
+				DeleteTeam();
+				ShowMenu();
+				break;
+			case 3:
+				ShowTeams();
+				ShowMenu();
+				break;
+			case 0:
+                 
+				exit(0);
+				break;
+			default:
+				ShowMenu();
+		}
+	}
 
-
-
-		return 0;
+return 0;
 }
